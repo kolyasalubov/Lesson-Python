a68d30ea951558f8b560cb53ff1282d51ee04248
IgorTomilov <67380519+IgorTomilov@users.noreply.github.com>
2020-07-07 19:54:38 +0300

    Delete Program (3).py

--- Program (3).py
+++
@@ -1,9 +0,0 @@
-#Четверте завдання третьої лекції Python
-
-number = str (input ("Введіть чотирьохзначне число: "))
-
-numb_list = list(number)
-numb_list.sort()
-
-
-print("Відсортовані цифри дорівнюють: ", numb_list)

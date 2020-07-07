cac7e7c43368c77362c0a4c08874523523ee1955
IgorTomilov <67380519+IgorTomilov@users.noreply.github.com>
2020-07-07 19:54:49 +0300

    Delete Program (2).py

--- Program (2).py
+++
@@ -1,9 +0,0 @@
-#Третє завдання третьої лекції Python
-
-number = str (input ("Введіть чотирьохзначне число: "))
-
-numb_list = list(number)
-numb_list.reverse()
-number1 = "".join(numb_list)
-
-print("'Оберенене' число дорівнюватиме: ", number1)
